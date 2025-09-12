4e618a9f95519902e59f91237f110eae03b56e26
Alejandro J. Cura <alecu@protocultura.net>
2025-09-12 13:38:14 -0300

    Demo mode

--- etec_led_control.ino
+++ etec_led_control.ino
@@ -3,20 +3,49 @@
 #define FIRST 2
 #define LAST 13
 
-void setup()
-{
-  SoftPWMBegin();
+#define DEMO 0
+#define OPERATION 1
 
+int mode;
+
+void all_to_zero()
+{
   for (int n = FIRST; n < LAST + 1; n++) {
     SoftPWMSet(n, 0);
   }
+}
+
+void setup()
+{
+  SoftPWMBegin();
+  all_to_zero();
 
   SoftPWMSetFadeTime(ALL, 100, 400);
   Serial.begin(9600);
-  Serial.println("Hola EspacioTec!");
+  Serial.println("=========================");
+  Serial.println("EspacioTec Led Controller");
+  Serial.println("-------------------------");
+  Serial.println("FORMA DE USO: recibe por serie lineas de texto terminadas con LF (\\n), con el formato \"LED VALUE\", ej: \"11 255\".");
+  Serial.println("Consultas? alecu@protocultura.net");
+  Serial.println("-------------------------");
+  mode = DEMO;
+  Serial.println("DEMO MODE");
 }
 
-void loop()
+void demo_loop()
+{
+  for (int n = FIRST; n < LAST + 1; n++) {
+    SoftPWMSet(n, random(32));
+  }
+  if (Serial.available() > 0) {
+    mode = OPERATION;
+    all_to_zero();
+    Serial.println("OPERATION MODE");
+  }
+  delay(1000);
+}
+
+void operation_loop()
 {
   while (Serial.available() > 0) {
     int pin = Serial.parseInt();
@@ -30,5 +59,17 @@ void loop()
       Serial.print("=");
       Serial.println(value);
     }
+  }
+}
+
+void loop()
+{
+  switch (mode) {
+    case DEMO:
+      demo_loop();
+      break;
+    case OPERATION:
+      operation_loop();
+      break;
   }
 }
